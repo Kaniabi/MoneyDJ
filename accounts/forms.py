2bc9d52a7ca490647c2610e151f3aae9cab840da
Joe Ray <joeray123@googlemail.com>
2010-01-20 09:54:28 +0000

    Adding a form for creating transactions

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,12 +1,20 @@
 # coding=utf8
-from django.forms import ModelForm
 from django import forms
-from moneydj.money.models import Account
+import datetime
+from moneydj.money.models import Account, Transaction
+from django.utils.translation import ugettext as _
 
-class AccountForm(ModelForm):
+class AccountForm(forms.ModelForm):
     currency = forms.ChoiceField(choices=[(u'£', u'GBP (£)'), (u'€', u'EUR (€)')])
     error_css_class = 'error'
     required_css_class = 'required'
     class Meta:
         model = Account
         exclude = ('user', 'starting_balance', 'balance_updated', 'date_created')
+
+class QuickTransactionForm(forms.Form):
+    date = forms.DateField(initial=datetime.date.today())
+    payee = forms.CharField(max_length=50)
+    amount = forms.DecimalField(decimal_places=2,max_digits=8)
+    credit = forms.ChoiceField(widget=forms.RadioSelect, choices=[(False, _(u'Expense')), (True, _(u'Income'))], initial=False)
+    transfer = forms.BooleanField(label=_(u'Transfer'), required=False)
--- accounts/views.py
+++ accounts/views.py
@@ -10,10 +10,40 @@ def view(request, id):
     """Lists the transactions in an account"""
     acc = get_object_or_404(Account, pk=id, user=request.user)
     
+    if (request.method == 'POST'):
+        transaction_form = QuickTransactionForm(request.POST)
+        if (transaction_form.is_valid()):
+            tr = Transaction()
+            # Try to find an existing payee with that name
+            payee = Payee.objects.filter(name__iexact=transaction_form.cleaned_data['payee'])
+            
+            if (len(payee) == 1):
+                payee = payee[0]
+            else:
+                # Create a new payee
+                payee = Payee(name=transaction_form.cleaned_data['payee'])
+                payee.save()
+                
+            tr.payee = payee
+                
+            tr.account = acc
+            tr.amount = transaction_form.cleaned_data['amount']
+            tr.transfer = transaction_form.cleaned_data['transfer']
+            tr.credit = transaction_form.cleaned_data['credit']
+            tr.date = transaction_form.cleaned_data['date']
+            tr.save()
+            transaction_form = QuickTransactionForm()
+    else:
+        transaction_form = QuickTransactionForm()
+    
     # Get all the transactions
     transactions = Transaction.objects.filter(account=acc).order_by('-date', '-date_created').select_related()
 
-    return render_to_response('account_view.html', {'account': acc, 'transactions': transactions}, context_instance = RequestContext(request))
+    return render_to_response('account_view.html', {
+        'account': acc, 
+        'transactions': transactions, 
+        'transaction_form': transaction_form
+    }, context_instance = RequestContext(request))
 
 def add(request):
     """Adds an account"""
