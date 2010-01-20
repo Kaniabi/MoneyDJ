e6c578abd24d92c726efc246f9f2d6d702b4c867
Joe Ray <joeray123@googlemail.com>
2010-01-20 19:11:00 +0000

    Adding middleware to handle the debug footer courtesy of Simon Willison

--- settings.py
+++ settings.py
@@ -65,6 +65,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'moneydj.money.debug_middleware.DebugFooter'
 )
 
 INTERNAL_IPS = (
