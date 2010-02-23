e3a2482d31683e3fcb9b87be97f7a4f155e20d46
Joe Ray <joeray123@googlemail.com>
2010-02-23 10:45:18 +0000

    Further abstracting the way transaction tables are rendered

--- accounts/templatetags/accounts_tags.py
+++ accounts/templatetags/accounts_tags.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.inclusion_tag("view_transactions.html")
 def show_transactions(transactions):
     return {'transactions': transactions}
+
+@register.inclusion_tag("transaction_row.html")
+def transaction_row(t):
+    return {'t': t}
