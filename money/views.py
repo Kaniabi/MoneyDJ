ce2ce69234c2a9c14bb3ce01d428b96c036b54bb
Joe Ray <joeray123@googlemail.com>
2010-03-19 16:31:38 +0000

    Redirect the home page if logged in

--- money/views.py
+++ money/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
+from django.core.urlresolvers import reverse
 
 def index(request):
+    if request.user:
+        return redirect(reverse('moneydj.dashboard.views.index'))
+    
     return render_to_response("index.html", context_instance=RequestContext(request))
