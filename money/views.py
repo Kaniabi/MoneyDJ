e509ae7418c42d62a13849a505afd18f022bcf60
Joe Ray <joeray123@googlemail.com>
2010-03-23 14:57:27 +0000

    Fixed a bug with the index which would always redirect users to the dashboard whether they're registered or not

--- money/views.py
+++ money/views.py
@@ -3,7 +3,7 @@ from django.template import RequestContext
 from django.core.urlresolvers import reverse
 
 def index(request):
-    if request.user:
+    if request.user.is_authenticated():
         return redirect(reverse('moneydj.dashboard.views.index'))
     
     return render_to_response("index.html", context_instance=RequestContext(request))
