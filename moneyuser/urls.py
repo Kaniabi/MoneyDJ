a4a02efab1b7e6f2ba0df1b49882d74e193a8ffb
Joe Ray <joeray123@googlemail.com>
2010-03-27 12:25:15 +0000

    Allowed users to register. Closes #24

--- moneyuser/urls.py
+++ moneyuser/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     (r'^login/$', 'django.contrib.auth.views.login'),
     (r'^logout/$', 'django.contrib.auth.views.logout'),
+    (r'^register/$', 'moneydj.moneyuser.views.register'),
     (r'^$', 'moneydj.moneyuser.views.profile')
     #(r'^transactions/$', 'transactions'),
 )
--- moneyuser/views.py
+++ moneyuser/views.py
@@ -1,7 +1,27 @@
-
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect, render_to_response
+from django.template.context import RequestContext
 
 # Create your views here.
 @login_required
 def profile(request):
     pass
+
+def register(request):
+    dashboard = reverse('moneydj.dashboard.views.index')
+    
+    if request.user.is_authenticated():
+        return redirect(dashboard)
+    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(dashboard)
+        
+    else:
+        form = UserCreationForm()
+        
+    return render_to_response('registration/register.html', { 'form': form }, context_instance=RequestContext(request))
