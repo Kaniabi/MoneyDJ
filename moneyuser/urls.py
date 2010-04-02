d306933aec776f67c970cdbae6940f1f8a265462
Joe Ray <joeray123@googlemail.com>
2010-04-02 09:02:33 +0100

    Fixing the logout page. Now it just redirects to the login page again. Closes #33

--- moneyuser/urls.py
+++ moneyuser/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^login/$', 'django.contrib.auth.views.login'),
-    (r'^logout/$', 'django.contrib.auth.views.logout'),
+    (r'^logout/$', 'django.contrib.auth.views.logout_then_login'),
     (r'^register/$', 'moneydj.moneyuser.views.register'),
     (r'^$', 'moneydj.moneyuser.views.profile')
     #(r'^transactions/$', 'transactions'),
