79ecc32d56c1539cd41591d62dd3bdb55852adce
Joe Ray <jr261@ex.ac.uk>
2010-05-01 17:07:45 +0100

    Added required/error classes for the login form

--- moneyuser/forms.py
+++ moneyuser/forms.py
@@ -1,5 +1,9 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 class RegisterForm(UserCreationForm):
+	error_css_class = 'error'
+	required_css_class = 'required'
+
+class LoginForm(AuthenticationForm):
 	error_css_class = 'error'
 	required_css_class = 'required'
--- moneyuser/urls.py
+++ moneyuser/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import *
+from moneyuser.forms import LoginForm
 
 urlpatterns = patterns('',
-    url(r'^login/$', 'django.contrib.auth.views.login', name='user-login'),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'authentication_form': LoginForm}, name='user-login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name='user-logout'),
     url(r'^register/$', 'moneydj.moneyuser.views.register', name='user-register'),
     url(r'^$', 'moneydj.moneyuser.views.profile', name='user-profile')
